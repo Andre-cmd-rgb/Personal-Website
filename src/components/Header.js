d7f41fd4ede058279e2885d3e16a7409842fe1ed
Andrew <andreavigolini@gmail.com>
2025-06-20 19:10:15 +0200

    speed insights

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link'; // 1. Import the Link component from Next.js
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const Header = () => {
   const title = "andre-cmd-rgb";
@@ -14,6 +15,7 @@ const Header = () => {
       <p style={{ color: '#888888', marginTop: '1rem', fontSize: '1.1rem' }}>
       This is my website 😅, don’t judge 🙈
       </p>
+    <SpeedInsights />
     </header>
   );
 };
